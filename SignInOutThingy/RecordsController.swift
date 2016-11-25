8fba84c01b155fbee6f4bf5d1dbdbb286fe668f6
Sweeper777 <sumulang@gmail.com>
2016-11-25 06:55:31 +0000

    showed records in table view

--- SignInOutThingy/RecordsController.swift
+++ SignInOutThingy/RecordsController.swift
@@ -1,9 +1,44 @@
 import UIKit
 
 class RecordsController: UITableViewController {
+    var entries: [Entry]?
+    
+    override func viewDidLoad() {
+        if let entries = CoreDataHelper.allEntries {
+            self.entries = entries
+        } else {
+            let alert = UIAlertController(title: "Error", message: "Failed to get records.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.presentVC(alert)
+        }
+    }
     
     @IBAction func signInOutBtnPressed(sender: AnyObject) {
         performSegue(withIdentifier: "signInOut", sender: self)
+    }
+    
+    @IBAction func unwindFromSignInOut(segue: UIStoryboardSegue) {
+        CoreDataHelper.updateData()
+        self.entries = CoreDataHelper.allEntries
+        tableView.reloadData()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return entries?.count ?? 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        cell.textLabel?.text = entries?[indexPath.row].description
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
 
