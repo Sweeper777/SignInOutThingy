b3305b1df36442b515ca8040111955ac78d42039
Sweeper777 <sumulang@gmail.com>
2016-11-26 08:51:14 +0000

    small changes

--- SignInOutThingy/SignInOutFormController.swift
+++ SignInOutThingy/SignInOutFormController.swift
@@ -7,7 +7,7 @@ class SignInOutFormController: FormViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        form +++ PickerInlineRow<String>(tagLocation) {
+        form +++ PushRow<String>(tagLocation) {
             row in
             row.title = "Location"
             row.options = ["Location 1", "Location 2", "Location 3", "Custom"]
@@ -18,7 +18,7 @@ class SignInOutFormController: FormViewController {
                 row.title = "Custom Location"
                 row.hidden = .function([tagLocation]) {
                     form in
-                    let locationRow: PickerInlineRow<String> = form.rowBy(tag: tagLocation)!
+                    let locationRow: PushRow<String> = form.rowBy(tag: tagLocation)!
                     return locationRow.value != "Custom"
                 }
         }
@@ -38,7 +38,7 @@ class SignInOutFormController: FormViewController {
         
 //        var presenceDict = [String: Bool]()
         for person in people {
-            let present = CoreDataHelper.hasPersonCompletedEntry(person: person)
+            let present = CoreDataHelper.hasPersonCompletedEntry(person: person, isVisitor: false)
             form.allSections.last! <<< CheckRow("\(tagPerson)_\(person)") {
                 row in
                 row.title = person
@@ -46,11 +46,15 @@ class SignInOutFormController: FormViewController {
                 row.hidden = .function([tagInOrOut]) {
                     form in
                     let inOrOutRow: SegmentedRow<String> = form.rowBy(tag: tagInOrOut)!
-                    if inOrOutRow.value == "In" && !present {
+                    if present == nil {
+                        return true
+                    }
+                    
+                    if inOrOutRow.value == "In" && !present! {
                         return false
                     }
                     
-                    if inOrOutRow.value == "Out" && present {
+                    if inOrOutRow.value == "Out" && present! {
                         return false
                     }
                     return true
@@ -72,7 +76,8 @@ class SignInOutFormController: FormViewController {
         
         let values = form.values(includeHidden: false)
         let location = (values[tagLocation] as? String == "Custom" ? values[tagCustomLocation] : values[tagLocation]) as? String
-        guard location != nil && location!.trimmed() != "" else {
+        let inOrOut = values[tagInOrOut] as! String
+        guard (location != nil && location!.trimmed() != "") || inOrOut == "In" else {
             showErrorMessage("Please select a location!")
             return
         }
