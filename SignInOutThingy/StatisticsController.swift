0268e3efe2df77e470ea14f050b35c16824fa0f6
Sweeper777 <sumulang@gmail.com>
2016-12-02 06:53:13 +0000

    added Statistics controller

---
+++ SignInOutThingy/StatisticsController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class StatisticsController: UITableViewController {
+    var person: String?
+    
+    @IBOutlet var wentOutTimes: UILabel!
+    @IBOutlet var beingVisitedTimes: UILabel!
+    @IBOutlet var totalTimeOutisde: UILabel!
+    @IBOutlet var totalTimeBeingVisited: UILabel!
+    @IBOutlet var forgotTimes: UILabel!
+    
+}
